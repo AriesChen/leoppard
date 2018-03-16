2ca4050d9d662264859149bc3cbd8df1d8677b9e
Aries Chen <7931218@qq.com>
2018-03-16 17:16:25 +0800

    frist commit

---
+++ JS/main.js
@@ -0,0 +1,37 @@
+(function () {
+
+  var bv = new Bideo();
+  bv.init({
+    // Video element
+    videoEl: document.querySelector('#background_video'),
+
+    // Container element
+    container: document.querySelector('body'),
+
+    // Resize
+    resize: true,
+
+    // autoplay: false,
+
+    isMobile: window.matchMedia('(max-width: 768px)').matches,
+    
+
+    // Array of objects containing the src and type
+    // of different video formats to add
+    src: [
+      {
+        src: '../test.mp4',
+        type: 'video/mp4'
+      }
+      // {
+      //   src: 'test.webm',
+      //   type: 'video/webm;codecs="vp8, vorbis"'
+      // }
+    ],
+
+    // What to do once video loads (initial frame)
+    onLoad: function () {
+      document.querySelector('#video_cover').style.display = 'none';
+    }
+  });
+}());
